6ed8cbcdbf48c85086664d5c75e0e7f638bd6ef1
JfuentesR <infojose20@gmail.com>
2021-03-29 15:53:26 +0200

    deleting BETA, merging with production

--- register.sh
+++ register.sh
@@ -1,10 +1,10 @@
 # First get a TOKEN
 # export TOKEN='<TOKEN>'
 # export AUTH="x-auth-token: $TOKEN"
-http POST http://paas:6000/bigdata/api/v1/products name=mariadb version=10.1.19-BETA description='mariadb is a free and open-source cross-platform document-oriented database.' logo_url='http://hadoop.cesga.es/img/mariadb-usa-inc.png' x-auth-token:$TOKEN
-curl -X PUT http://paas:6000/bigdata/api/v1/products/mariadb/10.1.19-BETA/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
-curl -X PUT http://paas:6000/bigdata/api/v1/products/mariadb/10.1.19-BETA/options --data-binary @options.json -H "$AUTH"
-curl -X PUT http://paas:6000/bigdata/api/v1/products/mariadb/10.1.19-BETA/orchestrator --data-binary @fabfile.py -H "$AUTH"
+http POST http://paas:6000/bigdata/api/v1/products name=mariadb version=10.1.19 description='mariadb is a free and open-source cross-platform document-oriented database.' logo_url='http://hadoop.cesga.es/img/mariadb-usa-inc.png' x-auth-token:$TOKEN
+curl -X PUT http://paas:6000/bigdata/api/v1/products/mariadb/10.1.19/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
+curl -X PUT http://paas:6000/bigdata/api/v1/products/mariadb/10.1.19/options --data-binary @options.json -H "$AUTH"
+curl -X PUT http://paas:6000/bigdata/api/v1/products/mariadb/10.1.19/orchestrator --data-binary @fabfile.py -H "$AUTH"
 
 # Launching
 # http POST http://paas:6000/bigdata/api/v1/products/mariadb/10.1.19 password="verysecret" x-auth-token:$TOKEN
