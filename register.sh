b524f8de9dd5720e078c6ceb30af335531ca8a8f
JfuentesR <infojose20@gmail.com>
2021-03-29 13:46:46 +0200

    BETA VERSION

--- register.sh
+++ register.sh
@@ -1,7 +1,7 @@
 # First get a TOKEN
 # export TOKEN='<TOKEN>'
 # export AUTH="x-auth-token: $TOKEN"
-http POST http://paas:6000/bigdata/api/v1/products name=mariadb version=10.1.19 description='mariadb is a free and open-source cross-platform document-oriented database.' logo_url='http://hadoop.cesga.es/img/mariadb-usa-inc.png' x-auth-token:$TOKEN
+http POST http://paas:6000/bigdata/api/v1/products name=mariadb version=10.1.19-BETA description='mariadb is a free and open-source cross-platform document-oriented database.' logo_url='http://hadoop.cesga.es/img/mariadb-usa-inc.png' x-auth-token:$TOKEN
 curl -X PUT http://paas:6000/bigdata/api/v1/products/mariadb/10.1.19/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
 curl -X PUT http://paas:6000/bigdata/api/v1/products/mariadb/10.1.19/options --data-binary @options.json -H "$AUTH"
 curl -X PUT http://paas:6000/bigdata/api/v1/products/mariadb/10.1.19/orchestrator --data-binary @fabfile.py -H "$AUTH"
