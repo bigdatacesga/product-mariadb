7990edc1e91762c6d15ccd1e14ac22ee1fb03b9f
JfuentesR <infojose20@gmail.com>
2021-10-29 14:44:57 +0200

    backup commit

--- Dockerfiles/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SERVICE=mariadb 
-VERSION=10.1.19
-
-read -p "Press [Enter] to build $SERVICE:$VERSION"
-docker build -t docker-registry.cesga.es:5000/$SERVICE:$VERSION .
-
-read -p "Press [Enter] key to push the image to the local registry"
-docker push docker-registry.cesga.es:5000/$SERVICE:$VERSION
